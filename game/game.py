33f0d8afcf2fdc2003cda4e4ca5f0b0ba210bd9c
Blackhole Diver <76709428+black-hole-diver@users.noreply.github.com>
2026-02-24 17:39:26 +0700

    save and load

--- game/game.py
+++ game/game.py
@@ -10,6 +10,9 @@ from .workers import Worker
 from .resource_manager import ResourceManager
 from .setting import INITIAL_WORKER, BACKGROUND_COLOR, WHITE, MAP_WIDTH
 
+import json
+import os
+
 
 class Game:
     def __init__(self, screen, clock):
@@ -40,6 +43,10 @@ class Game:
         # Stars
         self.background = self.create_starry_background()
         self.star_offset = 0  # for slow swirl animation
+
+        self.playing = False
+        self.notification_text = ""
+        self.notification_timer = 0
 
     def create_starry_background(self):
         surface = pg.Surface((self.width, self.height))
@@ -79,6 +86,10 @@ class Game:
                     self.quit_game()
                 if event.key == pg.K_SPACE:
                     self.paused = not self.paused
+                if event.key == pg.K_F5:
+                    self.save_game()
+                if event.key == pg.K_F9:
+                    self.load_game()
 
     def update(self):
         self.camera.update()
@@ -126,6 +137,19 @@ class Game:
             (10, 10)
         )
 
+        if self.notification_text != "":
+            if pg.time.get_ticks() - self.notification_timer < 3_000:
+                color = (255,50,50) if "Found" in self.notification_text else (50,255,50)
+                draw_text(
+                    self.screen,
+                    self.notification_text,
+                    50,
+                    color,
+                    (self.width // 2 - 120, 30)
+                )
+            else:
+                self.notification_text = ""
+
         pg.display.flip()
 
     def quit_game(self):
@@ -133,3 +157,107 @@ class Game:
         self.playing = False
         pg.quit()
         sys.exit()
+
+    def save_game(self, filename="savegame.json"):
+        print("Saving game...")
+        data = {
+            "resources": self.resource_manager.resources,
+            "camera": {"x": self.camera.scroll.x, "y": self.camera.scroll.y},
+            "map":[],
+            "buildings":[],
+            "workers":[]
+        }
+        for x in range(self.world.grid_length_x):
+            row = []
+            for y in range(self.world.grid_length_y):
+                row.append(self.world.world[y][x]["tile"])
+            data["map"].append(row)
+
+       # 2. Save Buildings
+        for x in range(self.world.grid_length_x):
+            for y in range(self.world.grid_length_y):
+                b = self.world.buildings[x][y]
+                if b is not None:
+                    data["buildings"].append({"name": b.name, "x": x, "y": y})
+
+        # 3. Save Workers
+        for x in range(self.world.grid_length_x):
+            for y in range(self.world.grid_length_y):
+                w = self.world.workers[x][y]
+                if w is not None:
+                    data["workers"].append({"x": x, "y": y})
+
+        # Write to JSON
+        with open(filename, "w") as f:
+            json.dump(data, f)
+        print("Game saved successfully!")
+        self.notification_text = "Game Saved!"
+        self.notification_timer = pg.time.get_ticks()
+
+    def load_game(self, filename="savegame.json"):
+        if not os.path.exists(filename):
+            print("No save file found!")
+            self.notification_text = "No save file found!"
+            self.notification_timer = pg.time.get_ticks()
+            return
+
+        print("Loading game...")
+        with open(filename, "r") as f:
+            data = json.load(f)
+
+        # 1. Restore Resources & Camera
+        self.resource_manager.resources = data["resources"]
+        self.camera.scroll.x = data["camera"]["x"]
+        self.camera.scroll.y = data["camera"]["y"]
+
+        # 2. Clear current entities and map data
+        self.entities.clear()
+        self.world.buildings = [[None for _ in range(self.world.grid_length_x)] for _ in
+                                range(self.world.grid_length_y)]
+        self.world.workers = [[None for _ in range(self.world.grid_length_x)] for _ in
+                              range(self.world.grid_length_y)]
+
+        # 3. Restore Map Tiles
+        for x in range(self.world.grid_length_x):
+            for y in range(self.world.grid_length_y):
+                tile_type = data["map"][x][y]
+                self.world.world[x][y]["tile"] = tile_type
+                # If there's a rock or tree, there's a collision
+                self.world.world[x][y]["collision"] = (tile_type != "")
+
+        # 4. Restore Buildings
+        for b_data in data["buildings"]:
+            name = b_data["name"]
+            x = b_data["x"]
+            y = b_data["y"]
+
+            render_pos = self.world.world[x][y]["render_pos"]
+            building_class = self.world.building_types.get(name)
+
+            if building_class:
+                # Grab the correct image from the HUD dictionary
+                image = self.hud.images.get(name)
+                ent = building_class(render_pos, image, self.resource_manager)
+
+                # BUG PREVENTION: The Building class automatically charges resources upon creation.
+                # Since we are loading an existing game, we must refund this cost.
+                for res, cost in self.resource_manager.costs.get(name, {}).items():
+                    self.resource_manager.resources[res] += cost
+
+                self.entities.append(ent)
+                self.world.buildings[x][y] = ent
+                self.world.world[x][y]["collision"] = True
+
+        # 5. Restore Workers
+        for w_data in data["workers"]:
+            x, y = w_data["x"], w_data["y"]
+            # Worker __init__ automatically appends to self.entities and self.world.workers
+            Worker(self.world.world[x][y], self.world)
+
+        # 6. Recalculate Collision Matrix for pathfinding
+        self.world.collision_matrix = self.world.create_collision_matrix()
+
+        self.notification_text = "Game Loaded!"
+        self.notification_timer = pg.time.get_ticks()
+
+        print("Game loaded successfully!")
